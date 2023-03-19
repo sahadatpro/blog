4bde30b745d9ae20dcbf01a6c158cb7c6cd6e891
sahadatpro <sahadatsays@gmail.com>
2023-03-19 16:29:04 +0600

    Post config

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publish', 'author', 'status']
+    search_fields = ['title', 'body']
+    prepopulated_fields = { 'slug': ('title', )}
+    # raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    list_filter = ['status', 'publish']
+
+admin.site.register(Post, PostAdmin) # Register
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from account.models import User
+from django.utils import timezone
 
-# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+class Post(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, null=True, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish', )
+
+    def __str__(self) -> str:
+        return self.title
